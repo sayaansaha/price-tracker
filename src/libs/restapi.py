f5275aed9132c343c37cbe34852c2752259d0aa7
Sayaan Saha <sayaan@leanplum.com>
2017-09-22 00:25:51 -0700

    Inital work on Rest Api lib.

--- src/libs/restapi.py
+++ src/libs/restapi.py
@@ -1,5 +1,7 @@
-import httplib2
+import urllib2
 import base64
+import os
+import json
 
 class RestApi(object):
   """docstring for RestApi"""
@@ -8,6 +10,24 @@ class RestApi(object):
     self.api_endpoint = api_endpoint
 
   def send_get(self, action, first_param=None, **kwargs):
-    """"""
+    """Sends a GET request"""
     uri = self.format_url(action, first_param, **kwargs)
+
+  def _send_request(self, method, uri, data=None):
+  	url = self.api_endpoint + uri
+  	request = urllib2.Request(url)
+  	if method == 'POST':
+  		request.add_data(json.dumps(data))
+  	request.add_header('Content-Type', 'application/json')
+  	request.add_header('User-Agent', 'lossless-app')
+  	try:
+  		response = urllib2.urlopen(request).read()
+  	except urllib2.HTTPError as e:
+  		response = e.read()
+  	if response:
+  		result = json.loads(response)
+  	else:
+  		result = {}
+  	return result
+  	
 
