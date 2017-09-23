3657b091c10d10e981324b2364dd6cf36be18191
Sayaan Saha <sayaan@leanplum.com>
2017-09-23 00:08:21 -0700

    RestApi lib almost complete.

--- src/libs/restapi.py
+++ src/libs/restapi.py
@@ -10,24 +10,85 @@ class RestApi(object):
     self.api_endpoint = api_endpoint
 
   def send_get(self, action, first_param=None, **kwargs):
-    """Sends a GET request"""
+    """
+    Issues a GET request (read) against the API and returns the result.
+
+    :param action: The API method to call.
+    :param first_param: First parameter we want to include if we need to.
+    :param kwargs: other parameters to include in our request.
+    :return: Response from the server (if we got one).
+
+    :type action: str
+    :type kwargs: dict
+    :return type: dict
+    """
     uri = self.format_url(action, first_param, **kwargs)
+    return self._send_request(method='GET', uri=uri)
+
+  def send_post(self, action, first_param=None, data=None):
+    """
+    Issues a Post request (read) against the API and returns the result.
+
+    :param action: The API method to call.
+    :param first_param: First parameter we want to include if we need to.
+    :param kwargs: other parameters to include in our request.
+    :return: Response from the server (if we got one).
+
+    :type action: str
+    :type kwargs: dict
+    :return type: dict
+    """
+    uri = self.format_url(action, first_param, **kwargs)
+    return self._send_request(method='POST', uri, data=None)
 
   def _send_request(self, method, uri, data=None):
-  	url = self.api_endpoint + uri
-  	request = urllib2.Request(url)
-  	if method == 'POST':
-  		request.add_data(json.dumps(data))
-  	request.add_header('Content-Type', 'application/json')
-  	request.add_header('User-Agent', 'lossless-app')
-  	try:
-  		response = urllib2.urlopen(request).read()
-  	except urllib2.HTTPError as e:
-  		response = e.read()
-  	if response:
-  		result = json.loads(response)
-  	else:
-  		result = {}
-  	return result
-  	
+    """
+    Issues a request to our Confluence API and returns the json response we got back.
+
+    :param method: Either 'POST' or 'GET'.
+    :param uri: The actual URL arguments for our request that we'll send up.
+    :param data: In the event of a post, we'll send this data with our request.
+    :return: Response from the server (if we got one).
+
+    :type method: str
+    :type uri: str
+    :type data: dict
+    :return type: dict
+    """
+    url = self.api_endpoint + uri
+    request = urllib2.Request(url)
+    if method == 'POST':
+      request.add_data(json.dumps(data))
+    request.add_header('Content-Type', 'application/json')
+    request.add_header('User-Agent', 'price-tracker')
+    try:
+      response = urllib2.urlopen(request).read()
+    except urllib2.HTTPError as e:
+      response = e.read()
+    if response:
+      result = json.loads(response)
+    else:
+      result = {}
+    return result
+  
+  def format_url(self, action, first_param=None, **kwargs):
+    """
+    Formats the string we append to our URL for API calls.
+    Necessary, because some API calls treat the first parameter as an end point in the url rather
+    than a url parameter.
+
+    :param action: String representing the API action we want to do.
+    :param end_point: The end point variable (if there is one).
+    :param kwargs: Additional url parameters that we want to attach to our API call.
+
+    :type action: str
+    :type end_point: str
+    :type kwargs: dict
+    :rtype: str
+    """
+    uri = action + '/'
+    if endpoint is not None:
+      uri = uri + endpoint
+    return uri
+    
 
